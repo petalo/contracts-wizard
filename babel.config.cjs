b41f8b8d0c2bb88a9f4d3fa2ddbe983cdfe16ad8
diegomarino <diegomarino@users.noreply.github.com>
2025-02-09 15:53:14 +0100

    chore: update test configuration files

--- babel.config.cjs
+++ babel.config.cjs
@@ -6,8 +6,12 @@ module.exports = {
         targets: {
           node: 'current',
         },
+        modules: 'commonjs',
       },
     ],
   ],
-  plugins: ['@babel/plugin-syntax-import-assertions'],
+  plugins: [
+    '@babel/plugin-syntax-import-assertions',
+    '@babel/plugin-syntax-dynamic-import',
+  ],
 };
